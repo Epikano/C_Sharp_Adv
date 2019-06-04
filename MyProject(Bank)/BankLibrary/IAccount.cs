c57cf8fb75156f3dcecefc2bf246c222c7c744d0
Nikita Zarubin <epikano@gmail.com>
2019-06-04 16:28:43 +0300

    MyProject(Bank)

---
+++ MyProject(Bank)/BankLibrary/IAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankLibrary
+{
+    public interface IAccount
+    {
+        void Put(decimal sum); // Положить деньги на счет
+        decimal Withdraw(decimal sum); // Взять со счета
+    }
+}
